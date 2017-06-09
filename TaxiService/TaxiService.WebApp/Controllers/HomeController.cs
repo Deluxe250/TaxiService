4e8bd817cec0158fca11f638cab35f27d504bb5e
Сергей <deluxe250@ya.ru>
2017-06-09 17:54:25 +0400

    Projects have been added

---
+++ TaxiService/TaxiService.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TaxiService.WebApp.Models;
+
+namespace TaxiService.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                if (HttpContext.User.IsInRole(Roles.User.ToString()))
+                {
+                    RedirectToAction("");
+                }
+                else if (HttpContext.User.IsInRole(Roles.Admin.ToString()))
+                {
+                    RedirectToAction("");
+                }
+                else if (HttpContext.User.IsInRole(Roles.Driver.ToString()))
+                {
+                    RedirectToAction("");
+                }
+                else if (HttpContext.User.IsInRole(Roles.Dispatcher.ToString()))
+                {
+                    RedirectToAction("");
+                }
+
+            }
+            return RedirectToAction("Account", "Login");
+        }
+    }
+}
